72f6f8a5907517c20ebb90ab4351a9f689dbdf63
Mahsa Dehghani <mahsa.dehghani.s@gmail.com>
2023-10-14 11:50:22 -0400

    Initial Commit

---
+++ PhotoOfTheDay/PhotoOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoOfTheDayApp.swift
+//  PhotoOfTheDay
+//
+//  Created by Mahsa Sanij on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
